a457ec66e1892950df48175355969a6d0144aabc
Anne Gentle <annegentle@justwriteclick.com>
2020-06-17 23:09:02 -0500

    Put Auth token in header; still getting Problems parsing JSON

--- create_webhook.py
+++ create_webhook.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 import configvar
-import sys
-from requests import Request, Session
 import json
+from requests import Request, Session
+import sys
 """
 Creates webhooks in a repo upon release using
 GitHub API v3 POST /repos/:owner/:repo/hooks
@@ -24,7 +24,6 @@ def get_webhook(gh_orgname, repo_name, gh_username, gh_api_key, gh_secret):
         print("Response text: {}".format(gethooks.text))
 
 def post_create_webhook(gh_orgname, repo_name, gh_username, gh_api_key, gh_secret):
-    """POST /repos/:owner/:repo/hooks"""
     api_uri = "https://api.github.com/repos/{}/{}/hooks".format(gh_orgname, repo_name)
     print("API endpoint: {}".format(api_uri))
     print("Username: {}".format(gh_username))
@@ -33,7 +32,12 @@ def post_create_webhook(gh_orgname, repo_name, gh_username, gh_api_key, gh_secre
 
     try:
         print("In the try block")
-        useragent = {'User-Agent': 'annegentle'}
+
+        headers = {'User-Agent': 'annegentle',
+                   'content-type': 'application/json',
+                   'Authorization': 'token {}'.format(gh_api_key)
+        }
+        print(headers)
         payload = {
                'name': 'web',
                'active': True,
@@ -46,10 +50,9 @@ def post_create_webhook(gh_orgname, repo_name, gh_username, gh_api_key, gh_secre
                            }
                }
         session = Session()
-        session.auth = (gh_username, gh_api_key)
-        makehooks = Request('POST', api_uri, data=payload, headers=useragent).prepare()
+        makehooks = Request('POST', api_uri, data=payload, headers=headers).prepare()
         resp = session.send(makehooks)
-        print(payload)
+        print(json.dumps(payload, indent=4))
         print(resp.status_code)
         print(resp.text)
         print(json.dumps(resp.json(), indent=4))
@@ -72,8 +75,8 @@ def main(args):
         repolist = f.readlines()
         for repo in repolist:
             repo_name = repo.rstrip('\n')
-            #response = get_webhook(configvar.gh_orgname, repo_name, configvar.gh_username, configvar.gh_api_key, configvar.gh_secret)
             print("Working on this repo: " + repo_name)
-            response = post_create_webhook(configvar.gh_orgname, repo_name, configvar.gh_username, configvar.gh_api_key, configvar.gh_secret)
+            #getresponse = get_webhook(configvar.gh_orgname, repo_name, configvar.gh_username, configvar.gh_api_key, configvar.gh_secret)
+            postresponse = post_create_webhook(configvar.gh_orgname, repo_name, configvar.gh_username, configvar.gh_api_key, configvar.gh_secret)
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
